eafec1b3532bd3acb23a31094a9ebd317582ae41
Anthony Mina <anthonymina7@yahoo.com>
2022-08-29 20:12:55 -0700

    added random edges need more accurarcy though

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,19 @@ var router = express.Router();
 const fs = require("fs");
 
 /* GET home page. */
-router.get("/", function (req, res, next) { 
+router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
 // api to get the initial graph json
 router.get("/json", function (req, res, next) {
-  var number = req.query.number;
+  var number = parseInt(req.query.number);
   var nodes = getNodes(number);
   var edges = getEdges(nodes, number);
   var graph = {
     nodes: nodes,
     edges: edges
   };
-  console.log(graph);
   res.json(graph);
 });
 
@@ -35,33 +34,47 @@ module.exports = router;
 
 
 // generates the nodes based on how many user wants
-function getNodes(number){
+function getNodes(number) {
   var nodes = [];
-  for(var i = 0; i < number; i++){
-      nodes[i] = {
-          id: i,
-          label: "node " + i
-      };
+  for (var i = 0; i < number; i++) {
+    nodes[i] = {
+      id: i,
+      label: "node " + i
+    };
   }
   return nodes;
 }
 
 // randomly generates the edges/relationships between nodes
-function getEdges(nodes, number){
+function getEdges(nodes, number) {
   var edges = [];
-  for(var i = 0; i < number; i++){
-    var rand = getRandom(number, i);
-    edges[i] = {
-      from: i,
-      to: rand
-    };
+  var rand;
+  var max = Math.floor(number * 0.5) + number;
+  let amountOfEdges = Math.floor(Math.random() * (max - number + 1) + number);
+  console.log(amountOfEdges);
+  // TODO: if you always want connected make initial graph i<number boring connect all
+  for (var i = 0; i < amountOfEdges; i++) {
+    if (i < number) {
+      rand = getRandom(number, i);
+      edges[i] = {
+        from: i,
+        to: rand
+      };
+    }
+    else{
+      edges[i] = {
+        from: Math.floor(Math.random() * number),
+        to: Math.floor(Math.random() * number)
+      };
+    }
   }
+
   return edges;
 }
 
-// gets a random integer that is not equal to the current node (i)
-function getRandom(number, i){
+// gets a random integer that is not equal to the current node (i) and between 0-number
+function getRandom(number, i) {
   var rand = Math.floor(Math.random() * number);
-  while(rand == i) rand = Math.floor(Math.random() * number);
+  while (rand == i) rand = Math.floor(Math.random() * number);
   return rand;
 }
